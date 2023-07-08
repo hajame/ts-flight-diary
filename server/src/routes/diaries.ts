2a8cbd90b274f14c3ceeba6bd47558e6d2848cf7
hajame <harri.mehtala@helsinki.fi>
2023-07-08 09:52:28 +0300

    Skeleton for GET diaries

--- server/src/routes/diaries.ts
+++ server/src/routes/diaries.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import diaryService from "../services/diaryService";
 
 const router = express.Router();
 
 router.get("/", (_req, res) => {
-  res.send("Fetching all diaries!");
+  res.send(diaryService.getEntries());
 });
 
 router.post("/", (_req, res) => {

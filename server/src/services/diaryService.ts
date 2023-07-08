b7000d59bb7b265d9e2aaf0be2a46706d6ce85e7
hajame <harri.mehtala@helsinki.fi>
2023-07-08 10:13:20 +0300

    convert data to DiaryEntries

--- server/src/services/diaryService.ts
+++ server/src/services/diaryService.ts
@@ -1,8 +1,6 @@
-import diaryData from "../../data/entries.json";
+import diaries from "../../data/entries/diaries";
 
 import { DiaryEntry } from "../types";
-
-const diaries: DiaryEntry[] = diaryData as DiaryEntry[];
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
